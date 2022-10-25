b0a0f2cd31e45d3e4148cd01390d4484d938ba0c
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-10-25 22:51:52 +0700

    change logo in navbar

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,6 @@
 import logo from "./logo3.png";
 import logo2 from "./logo3.svg";
 import logo3 from "./logo2.png";
+import logo4 from "./logo88.png";
 
-export { logo, logo2, logo3 };
+export { logo, logo2, logo3, logo4 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { logo, logo3 } from "../assets";
+import { logo, logo3, logo4 } from "../assets";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className=" w-full flex p-6 gap-4 justify-between items-center bg-slate-500">
       <div className="flex gap-2">
-        <img src={logo} alt="profile picture" className="flex items-center sm:w-[50px] w-[30px]" />
-        <img src={logo3} alt="name-profile" className="flex items-center sm:w-[100px] w-[60px]" />
+        <img src={logo4} alt="profile picture" className="flex items-center sm:w-[170px] w-[120px]" />
         {/* <p className="flex items-center text-xl text-white font-[grenze]">Luth-Company</p> */}
       </div>
       <ul className="list-none text-white flex justify-end items-center gap-3">
